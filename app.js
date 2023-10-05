2298c7ed06690aa57c738b786a599314710bcff0
NeiRib <dp44mj@gmail.com>
2023-10-05 19:32:54 +0200

    cum

---
+++ app.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/frase_aleatoria') {
+    fs.readFile(path.join(__dirname, 'frases.json'), 'utf8', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ error: 'Error del server' }));
+        return;
+      }
+      const frases = JSON.parse(data);
+      const fraseAleatoria = frases[Math.floor(Math.random() * frases.length)];
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ frase: fraseAleatoria }));
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ error: 'Ruta no encontrada' }));
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Funciona chevere en el puerto: ${PORT}`);
+});
